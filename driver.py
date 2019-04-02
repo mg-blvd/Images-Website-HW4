727945085c1385420a0e298937e48ecacabc6a8a
Misael Guijarro <misaeltguijarro@student.hartnell.edu>
2019-04-02 11:08:42 -0700

    Made the image selection code better

--- driver.py
+++ driver.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from image_info import image_info
 import random
 
 app = Flask(__name__, static_url_path = "/static", static_folder = "static")
+bootstrap = Bootstrap(app)
+
+def assert_no_repeating(array1):
+    current = random.choice(image_info)
+    while(current["title"] in array1):
+        current = random.choice(image_info)
+    return current
+
 
 @app.route('/')
 def main_page():
-    randoms = []
-    img1 = random.choice(image_info)
-    img2 = random.choice(image_info)
-    img3 = random.choice(image_info)
-    return render_template('home.html', first=img1, second=img2, third=img3)
+    past_images = []
+    current_images = [0,0,0]
+    for i in range(len(current_images)):
+        current_images[i] = assert_no_repeating(past_images)
+        past_images.append(current_images[i]["title"])
+
+    return render_template('home.html', first=current_images[0], second=current_images[1], third=current_images[2])
 
 if __name__ == '__main__':
     app.run(debug=True)
