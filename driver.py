9bd77c93559742cd13f90153cc5a73aa75ddd27f
Misael Guijarro <misaeltguijarro@student.hartnell.edu>
2019-04-02 22:22:33 -0700

    No More Repeating
    Added functionality that prevents the sae images from popping up when the page is refreshed.

--- driver.py
+++ driver.py
@@ -8,19 +8,29 @@ import ast
 app = Flask(__name__, static_url_path = "/static", static_folder = "static")
 bootstrap = Bootstrap(app)
 
-def assert_no_repeating(array1):
+def assert_no_repeating(array1, array2):
     current = random.choice(image_info)
-    while(current["title"] in array1):
+    while (current["title"] in array1 or current["title"] in array2):
         current = random.choice(image_info)
     return current
 
 @app.route('/')
 def main_page():
-    past_images = []
+    f = open("former_images.txt", "r")
+    past_images = f.read().split('\n')
+    f.close()
     current_images = []
-    for i in range(len(current_images)):
-        current_images.append(assert_no_repeating(past_images))
-        past_images.append(current_images[i]["title"])
+    current_image_titles = []
+    print(past_images)
+    for i in range(3):
+        current_images.append(assert_no_repeating(past_images, current_image_titles))
+        current_image_titles.append(current_images[i]["title"])
+        print(current_images)
+        print(current_image_titles)
+    f = open("former_images.txt", 'w')
+    for i in range(3):
+        f.write(current_image_titles[i] + '\n')
+    f.close()
     return render_template('home.html', first=current_images[0], second=current_images[1], third=current_images[2])
 
 @app.route('/image_info/<var>')
